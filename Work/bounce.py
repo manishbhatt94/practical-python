4a5654bbf16bd8c9795c70b2647a019ad9e9edc9
Manish Bhatt <manishbhatt594@gmail.com>
2025-09-08 12:40:56 +0530

    Add code till Section Intro / Numbers - Ex 1.10

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+REBOUND_HEIGHT_FRACT = 3 / 5
+
+curr_drop_height = 100  # in meters
+
+for i in range(10):
+    curr_drop_height *= REBOUND_HEIGHT_FRACT
+    print(i + 1, round(curr_drop_height, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,25 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 1000
+extra_payment_start_month = 5 * 12 + 1  # 61
+extra_payment_end_month = (5 + 4) * 12  # 108
+
+while principal > 0:
+    month = month + 1
+    principal = principal * (1 + rate / 12) - payment
+    if extra_payment_start_month <= month <= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+    total_paid = total_paid + payment
+    print(f"{month} {round(total_paid, 2)} {round(principal, 2)}")
+
+print("Total paid", total_paid)
+print("Months", month)
