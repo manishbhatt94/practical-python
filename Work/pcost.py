6e127a0704dcfb3f47c717b22c1230400025d735
Manish Bhatt <manishbhatt594@gmail.com>
2025-09-09 20:02:43 +0530

    Add Ex 1.27 - read CSV file & perform basic calculation on data

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open("Data/portfolio.csv") as file:
+    total = 0
+    _ = next(file)
+    for line in file:
+        _, qty, price = line.strip().split(",")
+        qty = int(qty)
+        price = float(price)
+        total += qty * price
+
+print("Total cost", round(total, 2))
