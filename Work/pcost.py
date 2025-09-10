91ebb30471313c20f2aadd32d96c66f6ad19e53f
Manish Bhatt <manishbhatt594@gmail.com>
2025-09-10 16:19:53 +0530

    Ex 1.31 Handle missing fields in CSV file using exception handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,19 +1,26 @@
 # pcost.py
-#
-# Exercise 1.27
 
 
 def portfolio_cost(filename):
+    """Calculates the total cost of a stock portfolio ∑ (shares * price),
+    given filename (path) of a portfolio CSV file containing
+    (name, shares, price) records
+    """
     with open(filename) as file:
         total = 0
         _ = next(file)
         for line in file:
             _, qty, price = line.strip().split(",")
-            qty = int(qty)
-            price = float(price)
-            total += qty * price
+            try:
+                qty = int(qty)
+                price = float(price)
+                total += qty * price
+            except ValueError:
+                print("Invalid record:", line)
+                continue
         return total
 
 
-cost = portfolio_cost("Data/portfolio.csv")
+# cost = portfolio_cost("Data/portfolio.csv")
+cost = portfolio_cost("Data/missing.csv")
 print("Total cost:", cost)
