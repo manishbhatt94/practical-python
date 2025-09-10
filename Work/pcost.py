fe823f35ccf223f3fd1288efc2b017ba18547422
Manish Bhatt <manishbhatt594@gmail.com>
2025-09-10 17:22:05 +0530

    Ex 1.33 Take portfolio CSV filepath from cmdline args, with a default file, if args not provided

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,6 +1,7 @@
 # pcost.py
 
 import csv
+import sys
 
 
 def portfolio_cost(filename):
@@ -23,6 +24,10 @@ def portfolio_cost(filename):
         return total
 
 
-cost = portfolio_cost("Data/portfolio.csv")
-# cost = portfolio_cost("Data/missing.csv")
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = "Data/portfolio.csv"
+
+cost = portfolio_cost(filename)
 print("Total cost:", cost)
